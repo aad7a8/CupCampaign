23ffe81cbd0289524934ca09d0f43fa7ba8d83fc
Astraea0821 <howardlee199951@gmail.com>
2026-03-03 16:04:38 +0800

    更新Ingredient資料表

--- backend/app/models.py
+++ backend/app/models.py
@@ -57,9 +57,8 @@ class Product(db.Model):
 class Ingredient(db.Model):
     __tablename__ = 'ingredient'
     id = db.Column(db.Integer, primary_key=True)
-    # tenant_id = db.Column(db.Integer, db.ForeignKey('tenant.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    monthly_status_matrix = db.Column(db.JSON, server_default='[]')
+    monthly_status_matrix = db.Column(db.JSON, server_default='[]') 
 
 # 6. 飲品組成表 (ProductComposition)
 # class ProductComposition(db.Model):
